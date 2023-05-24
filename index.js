b790b8fb9e81624707265316b8e6ce5e894ee882
Julietaorsi <julietaorsi2003@gmail.com>
2023-05-23 23:24:25 -0300

    TAREA 10

---
+++ index.js
@@ -0,0 +1,23 @@
+/*Variables*/
+
+/*strings*/
+
+let nombre= "Julieta"
+let apellido= "Orsi"
+
+/*numericas*/
+
+let numero= 33
+let numero2= 26
+let edad= 19
+
+let suma= numero + numero2
+let suma2= suma + edad
+let suma3= suma + 58
+
+console.log(nombre)
+console.log(apellido)
+console.log (nombre + "" + apellido)
+console.log (suma)
+console.log (suma2)
+console.log (suma3)

79f3801d4dfd28eab648ed80624ef7561db5a447
Julietaorsi <julietaorsi2003@gmail.com>
2023-05-25 13:31:53 -0300

    clase 11

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-/*Variables*/
+/*Clase 10*/
 
+/*Variables */
 /*strings*/
 
 let nombre= "Julieta"
@@ -21,3 +22,64 @@ console.log (nombre + "" + apellido)
 console.log (suma)
 console.log (suma2)
 console.log (suma3)
+
+/*Clase 11*/
+
+/*Ejercicio Heladeria IF*/
+
+let topping= 3
+
+if (topping=Oreo) {console.log (precio=$10);} 
+  
+else if (topping=Kitkat) {console.log (precio=$15)}
+
+else if (topping=Kinder) {console.log (precio=$25)}
+
+else {console.log ("No tenemos ese topping")}
+
+let helado= $700
+let precio= 0
+
+let preciofinal= helado+topping
+
+if (precioFinal) {console.log ("El helado cuesta $ + precioFinal")}
+
+console.log (precioFinal)
+console.log (topping)
+console.log (Oreo)
+console.log (Kitkat)
+console.log (Kinder)
+
+/*Ejercicio pedido Menú SWITCH*/
+
+const menu = 3;
+
+switch (menu) {case menu="carne": console.log("Bebida vino tinto");
+    break;
+  case menu= "pescado": console.log("Bebida vino blanco");
+   break;
+   case menu="verdura": console.log ("Bebida agua")
+  break;
+  default:
+    console.log("elija carne, pescado o verdura");
+    break; }
+
+console.log (menu)
+console.log (menucarne)
+console.log (menupescado)
+console.log (menuverdura)
+
+/*Ejercicio variable FOR*/
+
+let nombres= ["Julieta", "Juana","Julian"];
+for (let index = 0; index < nombres.length; index++) {
+  console.log("vuelta", index);
+  console.log(nombres[index]) }
+
+/*Ejercicio WHILE*/
+
+let count = 11;
+let contador = 0;
+while (count > contador) {
+  console.log("numero es mas chico que 11");
+  contador++;}
